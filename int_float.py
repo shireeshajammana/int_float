ce3dc738f0e0fce7e5cc84c56ac698a6ea9d48ff
shireeshajammana <109510692+shireeshajammana@users.noreply.github.com>
2022-07-19 21:13:37 +0530

    Add files via upload

---
+++ int_float.py
@@ -0,0 +1,38 @@
+# print(type(0))
+# print(type(4))
+# print(type(5756))
+# print(type(3453464575686797896))
+# print(type(-34534534534))
+
+# print(type(0.4))
+# print(type(.3))
+# print(type(546457.2342))
+# print(type(-5656.232))
+
+# a=7 ; b=8
+# print(a)
+# print('%d'%b)
+
+# c=6.7
+# print(c)
+# print('%f'%c)
+# d=7.82
+# print('%f'%d)
+# v=4.53
+# print('%.1f'%v)
+# print('%.2f'%v)
+# print('%.3f'%v)
+# print('%.4f'%v)
+
+''' Type Conversion & Casting '''
+# p=73232
+# print(type(p))
+# print(float(p))
+# print('%d'%p)
+# print('%f'%p)
+
+# q=5.89
+# print(type(q))
+# print(int(q))
+# print('%d'%q)
+# print('%f'%q)
